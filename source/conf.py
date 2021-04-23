3253b721faae1deebf68f9e0a0c09956c1c6ff4e
Laura Eroh <ikaheishi@ikaheishi.net>
2021-04-22 18:00:34 -0700

    Add foundation for a Sphinx project

---
+++ source/conf.py
@@ -0,0 +1,37 @@
+project = 'Telephone Bot'
+author = 'Telephone Bot team & contributors'
+copyright = '2018-2021 the Telephone Bot team & contributors'
+
+exclude_patterns = []
+templates_path = ['_templates']
+
+primary_domain = ''
+
+extensions = [
+	'sphinx.ext.extlinks',
+	'sphinx_rtd_theme',
+	'sphinxcontrib.httpdomain',
+	'sphinxemoji.sphinxemoji',
+]
+
+html_static_path = ['_static']
+html_extra_path = []
+html_title = "Telephone Bot User's Guide"
+html_short_title = "Telephone bot"
+#html_logo = ''
+#html_favicon = ''
+html_theme = 'sphinx_rtd_theme'
+html_theme_options = {
+	'display_version': False,
+}
+html_css_files=[
+	#
+]
+
+highlight_language = 'markdown'
+highlight_options = {
+	'php':{
+		'startinline': True
+	},
+}
+pygments_style = 'material'
